093ca60ae2742231b10d71df1eea516ce27db716
eddiemetrium <eddiemitch262@gmail.com>
2024-12-13 10:23:17 -0500

    calcular rust for terminal done

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env::{args, Args};
 fn main() {
     
     let mut args: Args = args();
-    
+
     let first = args.nth(1).unwrap();
     let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
@@ -16,26 +16,34 @@ fn main() {
 }
 
 fn operate(operator:char, first_number: f32, second_number: f32) -> f32 {
+    // if operator == '+' {
+    //  first_number + second_number
+    // }
+    
+    // else if operator == '-' {
+    //  first_number - second_number
+    // }
+    
+    // else if operator == '/' {
+    //  first_number / second_number
+    // }
+    
+    // else if operator == '*' {
+    //  first_number * second_number
+    // }
+    
+    // else {
+    //     0.0
+    // }
 
-if operator == '+' {
- first_number + second_number
-}
+    match operator {
 
-else if operator == '-' {
- first_number - second_number
-}
-
-else if operator == '/' {
- first_number / second_number
-}
-
-else if operator == '*' {
- first_number * second_number
-}
-
-else {
-    0.0
-}
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '*' | 'x' |'X' => first_number * second_number,
+        '/' => first_number / second_number,
+        _ => panic!("Invalid Operator!")
+    }
 
 } 
 
